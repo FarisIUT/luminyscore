2e0e0ec51fc28eece8cb7db9320253064dd692e1
Faris Desvignes <faris.desvignes@etu.univ-amu.fr>
2022-03-13 17:36:14 +0100

    fix main

--- luminyscoreserver/dist/getDataScores.js
+++ luminyscoreserver/dist/getDataScores.js
@@ -73,6 +73,10 @@ async function matchAdder() {
           let status = JSON.parse(response.body).response[u].fixture.status.long;
           let timestamp = JSON.parse(response.body).response[u].fixture.timestamp;
           let idSub = JSON.parse(response.body).response[u].fixture.id;
+          let stadium = JSON.parse(response.body).response[u].fixture.venue.name;
+          let league = JSON.parse(response.body).response[u].league.name;
+          let linkHome = JSON.parse(response.body).response[u].teams.home.logo;
+          let linkAway = JSON.parse(response.body).response[u].teams.away.logo;
           timestamps.push(timestamp);
           let goalsHome = JSON.parse(response.body).response[u].goals.home;
           let goalsAway = JSON.parse(response.body).response[u].goals.away;
@@ -114,7 +118,11 @@ async function matchAdder() {
               1: goalsAway
             },
             date: date,
-            timestamp: timestamp
+            timestamp: timestamp,
+            stadium: stadium,
+            league: league,
+            linkHome: linkHome,
+            linkAway: linkAway
           });
           matches2.push(idHome + "-" + idAway);
           idSub++;
@@ -143,6 +151,87 @@ async function matchAdder() {
   return matches;
 }
 
+async function matchById(id) {
+  let options = {
+    'method': 'GET',
+    'url': 'https://v3.football.api-sports.io/fixtures',
+    qs: {
+      id: id
+    },
+    'headers': {
+      'x-rapidapi-host': 'v3.football.api-sports.io',
+      'x-rapidapi-key': '0bed05814d17f569c5688a84a318f638'
+    }
+  };
+  await new Promise(next => {
+    var reponse;
+    request(options, async function (error, response) {
+      if (error) throw new Error(error);
+
+      for (let u = 0; u < JSON.parse(response.body).results; u++) {
+        let idHome = JSON.parse(response.body).response[u].teams.home.name;
+        let idAway = JSON.parse(response.body).response[u].teams.away.name;
+        let status = JSON.parse(response.body).response[u].fixture.status.long;
+        let timestamp = JSON.parse(response.body).response[u].fixture.timestamp;
+        let idSub = JSON.parse(response.body).response[u].fixture.id;
+        let stadium = JSON.parse(response.body).response[u].fixture.venue.name;
+        let league = JSON.parse(response.body).response[u].league.name;
+        let linkHome = JSON.parse(response.body).response[u].teams.home.logo;
+        let linkAway = JSON.parse(response.body).response[u].teams.away.logo;
+        let goalsHome = JSON.parse(response.body).response[u].goals.home;
+        let goalsAway = JSON.parse(response.body).response[u].goals.away;
+        let date = new Date(timestamp * 1000).toLocaleDateString("en-US");
+        let year = new Date(timestamp * 1000).getFullYear();
+        let month = new Date(timestamp * 1000).getMonth();
+        month++;
+        let day = new Date(timestamp * 1000).getDate();
+        let hours = new Date(timestamp * 1000).getHours();
+        let minutes = new Date(timestamp * 1000).getMinutes();
+
+        if (day < 10) {
+          day = "0" + day;
+        }
+
+        if (month < 10) {
+          month = "0" + month;
+        }
+
+        if (hours < 10) {
+          hours = "0" + hours;
+        }
+
+        if (minutes < 10) {
+          minutes = "0" + minutes;
+        }
+
+        date = day + "/" + month + "/" + year + " @ " + hours + ":" + minutes;
+        reponse = {
+          id: u,
+          idS: idSub,
+          equipe: {
+            0: idHome,
+            1: idAway
+          },
+          status: status,
+          score: {
+            0: goalsHome,
+            1: goalsAway
+          },
+          date: date,
+          timestamp: timestamp,
+          stadium: stadium,
+          league: league,
+          linkHome: linkHome,
+          linkAway: linkAway
+        };
+      }
+
+      next();
+    });
+  });
+  return reponse;
+}
+
 async function getEvents(idMatch) {
   var events;
   let options = {
@@ -169,6 +258,7 @@ async function getEvents(idMatch) {
 
 var _default = {
   matchAdder,
-  getEvents
+  getEvents,
+  matchById
 };
 exports.default = _default;
